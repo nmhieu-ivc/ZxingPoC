3f5c9b36278097b54871c907f396ee22fdcc3229
Nguyen Minh Hieu <nmhieu@vn.isb.co.jp>
2021-08-06 09:51:15 +0700

    Init project sample

---
+++ app/src/main/java/com/ivc/zxingpoc/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.ivc.zxingpoc
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var tvResultQR: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tvResultQR = findViewById(R.id.tvResult)
+        // init QR
+        val intentIntegrator = IntentIntegrator(this).apply {
+            setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+            setPrompt("")
+            setCameraId(0)
+            setBeepEnabled(true)
+            setOrientationLocked(true)
+        }
+        // load web view
+        findViewById<WebView>(R.id.webViewMain).loadUrl("https://www.google.com/")
+
+        // button QR
+        findViewById<Button>(R.id.btQR).setOnClickListener {
+            // clear text result
+            tvResultQR.text = ""
+            zxingActivityResultLauncher.launch(intentIntegrator.createScanIntent())
+        }
+
+        // button Send
+        (findViewById<Button>(R.id.btSend)).setOnClickListener {
+            Toast.makeText(this, "Button SEND is clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val zxingActivityResultLauncher  = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        val intentResult = IntentIntegrator.parseActivityResult(it.resultCode, it.data)
+        if(intentResult.contents != null) {
+            tvResultQR.text = intentResult.contents
+        }
+    }
+
+}
